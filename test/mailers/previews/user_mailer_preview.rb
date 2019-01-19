4b1b3adca50d8a33279e1cf6d3cb619bd4c3d904
ethenalee <yeonji900111@gmail.com>
2019-01-19 05:30:39 +0000

    refactored spacing and add descriptions

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,5 +1,5 @@
 class UserMailerPreview < ActionMailer::Preview
-
+  # usermailer preview page  http://localhost:3000/rails/mailers/user_mailer/order_email
   def order_email
     UserMailer.order_email(User.first, Order.first)
   end
