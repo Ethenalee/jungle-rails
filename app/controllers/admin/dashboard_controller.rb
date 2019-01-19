acb583fe48705ff4af5074528cfa8e1b94f26bbf
ethenalee <yeonji900111@gmail.com>
2019-01-19 03:01:43 +0000

    add total users and order in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < Admin::BaseController
-    
+
+  # admin_root GET  /admin(.:format) admin/dashboard#show
   def show
+    @users = User.all
+    @orders = Order.all
   end
 
 end
