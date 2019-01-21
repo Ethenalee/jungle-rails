23c5c04c41f940e270de9473253f3fc964955ad3
ethenalee <yeonji900111@gmail.com>
2019-01-21 20:55:18 +0000

    email case validation/password mim&max length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,24 @@
 class User < ActiveRecord::Base
+
+  before_validation :downcase_email
+  
   has_secure_password
-  validates :email, uniqueness: true
-  validates :name, :check_name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :name, presence: true
+  validate :check_name
+  validates :password, presence: true, length: {:within => 6..12}
+  validates :password_confirmation, presence: true
+
+  private 
 
   def check_name
     unless name.split.size > 1
       errors.add(:name, 'Need first and last name')
     end
+  end
+
+  def downcase_email
+    self.email = email.downcase if email.present?
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,10 +12,92 @@ RSpec.describe User, type: :model do
         record.valid?
         expect(record.errors[:name]).to include "Need first and last name"
 
+        record.name = 'foo boo'
+        record.valid?
+        expect(record.errors[:name]).not_to include 'AError'
       end
     end
 
-    # describe '.authenticate_with_credentials' do
-    #   # examples for this class method here
-    # end
+    it 'has a password' do
+      record = User.new
+      record.name = 'foo boo'
+      record.password = '' # invalid state
+      record.valid?
+      expect(record.errors[:password]).to include "can't be blank"
+
+      record.password = 'fooboo'
+      record.valid?
+      expect(record.errors[:password]).not_to include "can't be blank"
+
+      record.password_confirmation = '' # invalid state
+      record.valid?
+      expect(record.errors[:password_confirmation]).to include "can't be blank"
+
+      record.password_confirmation = 'foo' # invalid state
+      record.valid?
+      expect(record.errors[:password_confirmation]).to include "doesn't match Password"
+
+      record.password_confirmation = 'fooboo' # invalid state
+      record.valid?
+      expect(record.errors[:password_confirmation]).not_to include 'AError'
+    end
+
+    it 'has a email' do
+      record = User.new
+      record.name = 'foo boo'
+      record.password = 'fooboo'
+      record.password_confirmation = 'fooboo'
+
+      record.email = '' # invalid state
+      record.valid?
+      expect(record.errors[:email]).to include "can't be blank"
+
+      record1 = User.new
+      record1.name = 'foo boo'
+      record1.password = 'fooboo'
+      record1.password_confirmation = 'fooboo'
+      record1.email = 'test@text.com' # invalid state
+      saved = record1.save
+      expect(saved).to eql(true)
+      record1.valid?
+      expect(record1.errors[:email]).not_to include 'AError'
+
+      record2 = User.new
+      record2.name = 'foo boo'
+      record2.password = 'fooboo'
+      record2.password_confirmation = 'fooboo'
+      record2.email = 'test@text.com' # invalid state
+      record2.valid?
+      expect(record2.errors[:email]).to include 'has already been taken'
+
+      record2 = User.new
+      record2.name = 'foo boo'
+      record2.password = 'fooboo'
+      record2.password_confirmation = 'fooboo'
+      record2.email = 'TEST@text.com' # invalid state
+      record2.valid?
+      expect(record2.errors[:email]).to include 'has already been taken'
+
+    end
+
+    it 'password length' do
+      record = User.new
+      record.name = 'foo boo'
+      record.password = 'fffff' # invalid state
+      record.valid?
+      expect(record.errors[:password]).to include "is too short (minimum is 6 characters)"
+
+      record.password = 'fffffffffffff' # invalid state
+      record.valid?
+      expect(record.errors[:password]).to include "is too long (maximum is 12 characters)"
+
+      record.password = 'fooboo' # invalid state
+      record.valid?
+      expect(record.errors[:password]).not_to include 'AError'
+
+    end
+
+    describe '.authenticate_with_credentials' do
+      # examples for this class method here
+    end
 end
